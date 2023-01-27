e66e59f85e324bade545c73ae14bc2b38f9fff33
Christian <c.espinoza.code@gmail.com>
2023-01-27 11:03:46 -0500

    auth header validation added

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,11 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
-
+import { Request } from 'express';
+// The guard will be injected any controller
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    // Access granted: true - Access denied: false
+
+    // Get request
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Auth');
+    const isAuth = authHeader === '123';
+    if (!isAuth) {
+      throw new UnauthorizedException(
+        'not allow, auth header does not match expected',
+      );
+    }
+    return isAuth;
   }
 }
