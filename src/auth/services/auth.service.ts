8cd29e24163c122f08c866b058f2b1b762e580f1
Christian <c.espinoza.code@gmail.com>
2023-01-28 21:28:27 -0500

    jwtService injected in auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { UsersService } from 'src/users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { PayloadToken } from '../models/token.models';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(userEmail: string, userPassword: string) {
     const user = await this.userService.findByEmail(userEmail);
@@ -18,5 +24,13 @@ export class AuthService {
     }
     const { password, ...rta } = user.toJSON();
     return rta;
+  }
+
+  async generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
