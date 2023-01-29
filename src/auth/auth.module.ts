cd7d5c3c8a2c9aa0e9cc34c7ed8f43708739799f
Christian <c.espinoza.code@gmail.com>
2023-01-28 22:53:24 -0500

    local strategy implemented in authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from './services/auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
-  providers: [AuthService, UsersModule],
+  imports: [PassportModule, UsersModule],
+  // Implementing Local strategy
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
