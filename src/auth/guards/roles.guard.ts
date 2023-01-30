0aecf03cc0f71dbfd794a3a57c2a6ccaf9c5c586
Christian <c.espinoza.code@gmail.com>
2023-01-30 11:09:00 -0500

    canActivate method implemented

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,11 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../models/roles.models';
+import { PayloadToken } from '../models/token.models';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const roles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler());
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as PayloadToken;
+    const isAuth = roles.some((role) => role === user.role);
+    if (!isAuth) {
+      throw new UnauthorizedException('access denied, role not allowed');
+    }
+    return isAuth;
   }
 }
