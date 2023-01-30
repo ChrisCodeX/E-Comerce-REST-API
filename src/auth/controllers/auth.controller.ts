0fde92ffa00bc7e63dd0d7d89cb64f917298f4c5
Christian <c.espinoza.code@gmail.com>
2023-01-29 22:03:05 -0500

    generateJWT method implemented in auth.controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 
+import { User } from 'src/users/entities/user.entity';
+import { AuthService } from '../services/auth.service';
+
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('/login')
   async login(@Req() req: Request) {
-    console.log('Yara xd');
-    console.log(req.user);
-    return req.user;
+    const user = req.user;
+    return await this.authService.generateJWT(user as User);
   }
 }
