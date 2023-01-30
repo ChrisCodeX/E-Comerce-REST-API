86e258f3c4e3e365ef6826905d3694c219fdc4f0
Christian <c.espinoza.code@gmail.com>
2023-01-29 22:50:50 -0500

    jwt strategy contructor & property configured

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+import config from 'config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
-    super({});
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.jwtSecret,
+    });
   }
 
-  async validate() {}
+  validate() {}
 }
