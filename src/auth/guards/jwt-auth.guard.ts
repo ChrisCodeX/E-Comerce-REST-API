0caebb9480d74b5c5eca8a63d3695a02ed86d0e7
Christian <c.espinoza.code@gmail.com>
2023-01-30 17:43:34 -0500

    jwt-auth guard validation added

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,26 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+// Auth guard wich extends from jwt strategy
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  // Sustituyendo el método canActivate de AuthGuard por una validación
+  // Si en los metadatos se encuentra el PUBLIC_KEY, lo deja pasar.
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
   }
 }
